15b381bb3571d5ed60a844fd3fdb18aedaa4b4da
IsmaeilSaberi <ismaeilsaberi@gmail.com>
2025-11-03 19:35:46 -0800

    implmenting better auth and login with github and neon db

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,17 @@
+import { Button } from "@/components/ui/button";
 import { ThemeToggle } from "@/components/ui/themeToggle";
-import Image from "next/image";
-export default function Home() {
+import { auth } from "@/lib/auth";
+import { headers } from "next/headers";
+
+export default async function Home() {
+  const session = await auth.api.getSession({
+    headers: await headers(),
+  });
   return (
-    <div>
+    <div className="p-24">
       <h1 className="text-red-500">Hello World</h1>
       <ThemeToggle />
+      {session ? <p>{session.user.name}</p> : <Button>Login</Button>}
     </div>
   );
 }
---
+++ prisma.config.ts
@@ -0,0 +1,13 @@
+import "dotenv/config";
+import { defineConfig } from "prisma/config";
+
+export default defineConfig({
+  schema: "prisma/schema.prisma",
+  migrations: {
+    path: "prisma/migrations",
+  },
+  engine: "classic",
+  datasource: {
+    url: process.env.DATABASE_URL!, // 👈 از process.env استفاده کن، نه env()
+  },
+});
