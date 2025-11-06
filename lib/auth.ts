86c3ff7f7c0c0aa96303e3a8b4fdd73b8a93734c
IsmaeilSaberi <ismaeilsaberi@gmail.com>
2025-11-06 04:01:48 -0800

    auth with email and better auth completed and now working well

--- lib/auth.ts
+++ lib/auth.ts
@@ -2,6 +2,8 @@ import { betterAuth } from "better-auth";
 import { prismaAdapter } from "better-auth/adapters/prisma";
 import { prisma } from "./db";
 import { env } from "./env";
+import { emailOTP } from "better-auth/plugins";
+import { resend } from "./resend";
 
 export const auth = betterAuth({
   database: prismaAdapter(prisma, {
@@ -13,4 +15,16 @@ export const auth = betterAuth({
       clientSecret: env.AUTH_GITHUB_SECRET as string,
     },
   },
+  plugins: [
+    emailOTP({
+      async sendVerificationOTP({ email, otp }) {
+        await resend.emails.send({
+          from: "LMS Studio <onboarding@resend.dev>",
+          to: [email],
+          subject: "LMS Studio - Verify your email",
+          html: `<p>Your verification code is: <strong>${otp}</strong></p>`,
+        });
+      },
+    }),
+  ],
 });
